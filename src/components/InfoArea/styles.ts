ddf3436ead530e0e6dcae3dd3f8e2cb83c63157f
Kelvin <kelvinrev2@hotmail.com>
2022-06-07 23:30:52 -0300

    Second Commit (next and prev date created)

---
+++ src/components/InfoArea/styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: white;
+    box-shadow: 10px;
+    border-radius: 10px;
+    padding: 20px;
+    margin-top: -40px;
+    display: flex;
+    align-items: center;
+`
+
+export const MonthArea = styled.div`
+    flex: 1;
+    display:flex;
+    align-items: center;    
+`
+
+export const MonthArrow = styled.div`
+    width: 40px;
+    text-align: center;
+    font-size: 25px;
+    cursos: pointer;
+    background-color: #00FFFF;
+    border: 1px solid grey;
+`
+
+export const MonthTitle = styled.div`
+    flex:1;
+    text-align: center;
+`
+
+export const ResumeArea= styled.div`
+    flex: 2;
+    display: flex;
+`
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,7 +1,7 @@
 import { Item } from "../types/Item"
 
 export const items: Item[] = [
-    { date: new Date(2022, 5, 15), category: 'food', title: 'McDonalds', value: 32.12},
-    { date: new Date(2022, 5, 15), category: 'rent', title: 'Aluguel', value: 300},
+    { date: new Date(2022, 5, 5), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date(2022, 5, 5), category: 'rent', title: 'Aluguel', value: 300},
     { date: new Date(2022, 5, 15), category: 'salary', title: 'VF', value: 1200},
 ]
